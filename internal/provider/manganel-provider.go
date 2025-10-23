0f6033e7077fc419a2afad9068022a8f5b4120f0
Ivan Penchev <ivan@penchev.com>
2025-10-23 13:16:54 +0200

    fix: specify to use sandbox

--- internal/provider/manganel-provider.go
+++ internal/provider/manganel-provider.go
@@ -20,8 +20,20 @@ func NewMangaNelProviderFactory(mangaNelGraphQLEndpoint string) func() (Provider
 
 		ctx, cancel := context.WithTimeout(context.Background(), 45*time.Second)
 		defer cancel()
-		innerCtx, _ := chromedp.NewContext(ctx)
 
+		opts := append(chromedp.DefaultExecAllocatorOptions[:],
+			chromedp.Flag("no-sandbox", true),
+			chromedp.Flag("headless", true),
+			chromedp.Flag("disable-gpu", true),
+			chromedp.Flag("disable-dev-shm-usage", true),
+		)
+
+		allocCtx, cancelAlloc := chromedp.NewExecAllocator(ctx, opts...)
+		defer cancelAlloc()
+
+		// Create the chromedp context from the allocator
+		innerCtx, cancelInner := chromedp.NewContext(allocCtx)
+		defer cancelInner()
 		randomPageFromProviderToOpen := "https://manganel.me/manga/my-wife-is-a-demon-queen"
 
 		// navigate to a page, wait for an element, click
