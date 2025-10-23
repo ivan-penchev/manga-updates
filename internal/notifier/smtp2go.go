147a96447e5e4bae147e21585ed90bc4c6927480
Ivan Penchev <ivan@penchev.com>
2025-10-23 18:26:39 +0200

    chore: linter make me happy

--- internal/notifier/smtp2go.go
+++ internal/notifier/smtp2go.go
@@ -96,7 +96,7 @@ func (s smtp2goNotifier) sendAPIRequest(endpoint string, requestBody io.Reader) 
 	if err != nil {
 		return nil, fmt.Errorf("failed to send HTTP request: %w", err)
 	}
-	defer res.Body.Close()
+	defer func() { _ = res.Body.Close() }()
 
 	ret := new(Smtp2goApiResult)
 	err = json.NewDecoder(res.Body).Decode(ret)
@@ -135,8 +135,8 @@ func (s smtp2goNotifier) NotifyForNewChapter(chapter types.ChapterEntity, fromMa
 		email.TemplateID = s.config.templateID
 		email.TemplateData = templateData
 	} else {
-		htmlBody = fmt.Sprintf("<h1>%s Update!</h1><p>Chapter %s is now available.</p><p>Read it here: <a href=\"%s\">%s</a></p>", fromManga.Name, chapter.Number, chapter.URI, chapter.URI)
-		textBody = fmt.Sprintf("%s Update! Chapter %s is now available. Read it here: %s", fromManga.Name, chapter.Number, chapter.URI)
+		htmlBody = fmt.Sprintf("<h1>%s Update!</h1><p>Chapter %.0f is now available.</p><p>Read it here: <a href=\"%s\">%s</a></p>", fromManga.Name, chapter.Number, chapter.URI, chapter.URI)
+		textBody = fmt.Sprintf("%s Update! Chapter %.0f is now available. Read it here: %s", fromManga.Name, chapter.Number, chapter.URI)
 		email.HtmlBody = htmlBody
 		email.TextBody = textBody
 	}
