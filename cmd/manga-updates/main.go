2ddf7625fcef94e1771c8d4d4b1270339f28c20a
Ivan Penchev <ivan@penchev.com>
2026-01-31 09:35:21 +0100

    chore: default to different env var

--- cmd/manga-updates/main.go
+++ cmd/manga-updates/main.go
@@ -15,7 +15,7 @@ import (
 )
 
 type config struct {
-	MangaNelGraphQLEndpoint    string `env:"API_ENDPOINT" envDefault:"https://manganel.me/api/graphql"`
+	MangaNelGraphQLEndpoint    string `env:"API_ENDPOINT" envDefault:"https://api.mghcdn.com/graphql"`
 	SeriesDataFolder           string `env:"SERIES_DATAFOLDER" envDefault:"$HOME/repos/manga-updates/data" envExpand:"true"`
 	SendGridAPIKey             string `env:"SENDGRID_API_KEY"`
 	SendGridTemplateId         string `env:"SENDGRID_TEMPLATE_ID"`
